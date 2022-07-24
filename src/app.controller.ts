8fd9227ac6716f1736660503377060c22f6107fd
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-07-24 18:47:39 -0500

    Creacion y detalle de pedido

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Response} from "express";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('pedido')
-  creacionPedido(@Body() body) {
-    console.log(body);
+  creacionPedido(@Body() body, @Res() response: Response) {
+    //primero se crea el pedido
+    this.appService.registrarPedido(body)
+        .subscribe({
+          next: (returnedInfo) =>{
+            let registerId = returnedInfo.data.id;
+            let detallePedido: any[] = body.tabla;
+            this.appService.registrarDetallePedido(registerId,detallePedido);
+            response.status(201).send({"idPedido": registerId});
+          }
+        })
+
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,49 @@ export class AppService {
   private readonly apiKey: string = "3809ccdc104d405f5387031f81abcf9cadba6f1767fcdff3";
   constructor(private readonly httpClient:HttpService) {
   }
-  getHello(): string {
-    return 'Hello World!';
+  registrarPedido(datosCliente){
+    let url = `https://app.flokzu.com/flokzuopenapi/api/${this.apiKey}/database/orden_pedido_com`;
+    let registroBase = {
+      "nombre_cliente": datosCliente.nombreCliente,
+      "ruc_cliente": datosCliente.ruc,
+      "ciudad": datosCliente.ciudad,
+      "provincia": datosCliente.provincia,
+      "direccion": datosCliente.direccion,
+      "fecha_entrega": datosCliente.fechaEntrega,
+      "estado": "sin_confirmar"
+    };
+    return this.httpClient.put(url,registroBase,{headers:{
+        'Content-Type': 'application/json',
+        'X-Api-Key': this.apiKey,
+        'X-Username': 'cesar.leon03@epn.edu.ec'
+      }})
+  }
+
+  registrarDetallePedido(id, datosTabla:any[]){
+    let url = `https://app.flokzu.com/flokzuopenapi/api/${this.apiKey}/database/orden_pedido_detalle_com`;
+    datosTabla.forEach(
+        (producto)=>{
+          let registroBase = {
+            "id_pedido": id,
+            "nombre_producto": producto.Nombre,
+            "codigo_producto": producto["Código"],
+            "cantidad_producto": producto.Cantidad,
+            "precio_producto": producto["Precio Unitario"]
+          };
+          this.httpClient
+              .put(
+              url,
+              registroBase,
+              {headers:{
+                  'Content-Type': 'application/json',
+                  'X-Api-Key': this.apiKey,
+                  'X-Username': 'cesar.leon03@epn.edu.ec'
+                }}
+              )
+              .subscribe()
+        }
+    );
+
   }
 }
+
